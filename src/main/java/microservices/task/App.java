3606ff4831efc63b7140bf0e17ec4b6844b2a25f
george <e.tihonov@websoft.ru>
2025-04-09 22:21:04 +0300

    Task 4 v1

--- src/main/java/microservices/task/App.java
+++ src/main/java/microservices/task/App.java
@@ -1,7 +1,23 @@
 package microservices.task;
 
+import microservices.task.model.User;
+import microservices.task.service.UsersApiService;
+import org.springframework.http.*;
+
 public class App {
     public static void main( String[] args ) {
+
+        User tester = new User(3L,"James","Brown", (byte) 33);
+
+        UsersApiService usersApiService = new UsersApiService();
+        HttpHeaders curSessionHeaders = usersApiService.getCurSessionHeaders();
+        String addUserCode = usersApiService.addUser(tester, curSessionHeaders).getBody();
+        tester.setName("Thomas");
+        tester.setLastName("Shelby");
+        String editUserCode = usersApiService.updateUser(tester, curSessionHeaders).getBody();
+        String deleteUserCode = usersApiService.deleteUser(tester, curSessionHeaders).getBody();
+
+        System.out.println("Final code: "+addUserCode+editUserCode+deleteUserCode);
 
     }
 }
